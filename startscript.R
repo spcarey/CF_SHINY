7765aa67bad9674666aa7f1565baf78cfdde8189
spcarey <sean.carey@careys.io>
2019-01-24 15:34:49 -0800

    *change file structure *edit startscript.R

--- startscript.R
+++ startscript.R
@@ -8,4 +8,4 @@ require(DT)
 
 port <- Sys.getenv('PORT') 
 
-shiny::runApp('R_TEST_APP', host = '0.0.0.0', port = as.numeric(port))
+shiny::runApp('SHINY_APP',  host="0.0.0.0", port=as.numeric(Sys.getenv("PORT")))
